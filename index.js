2632bb5e37a0d9ed691e5d488b5b86ee92ed5974
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-10 17:20:31 +0300

    top sale section added

--- index.js
+++ index.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
         dots: true,
         items: 1
     });
+
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
 });
