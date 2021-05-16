9c924407d7837e851d26178d94b6ad0073d2ae25
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-16 12:09:08 +0300

    php templates

---
+++ cart.php
@@ -0,0 +1,21 @@
+<?php
+  // include header.php file
+  include("header.php");
+?>
+
+<?php
+
+    /* include cart */
+      include("template/_cart.php");
+    /* include cart */
+
+    /* include new phones */
+        include("template/_new_phones.php");
+    /* include banner area */
+
+?>
+
+<?php
+  // include footer.php file
+  include("footer.php");
+?>
---
+++ product.php
@@ -0,0 +1,23 @@
+<?php
+  // include header.php file
+  include("header.php");
+?>
+
+
+<?php
+
+    /* include products */
+        include("template/_products.php");
+    /* include products */
+
+    /* include top sale */
+        include("template/_top_sale.php");
+    /* include banner area */
+
+?>
+
+
+<?php
+  // include footer.php file
+  include("footer.php");
+?>
