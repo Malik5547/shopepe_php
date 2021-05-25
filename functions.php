1bb6ae13968639dc917c71954b65a56136d1962e
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-25 19:29:44 +0300

    add to cart

--- functions.php
+++ functions.php
@@ -6,6 +6,8 @@ require('database/DBController.php');
 // require Product class
 require('database/product.php');
 
+// require Cart Class
+require('database/Cart.php');
 
 // DBController object
 $db = new DBController();
@@ -13,4 +15,9 @@ $db = new DBController();
 // Product object
 $product = new Product($db);
 $product_shuffle = $product->getData();
+
+// Cart object
+$Cart = new Cart($db);
+
+
 ?>
--- template/_new_phones.php
+++ template/_new_phones.php
@@ -1,4 +1,16 @@
 <!-- New Phones -->
+
+<?php
+  // request method post
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_POST['new_phones_submit'])){
+      // call method addToCart
+      $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+    }
+  }
+?>
+
+
 <section id="new-phones">
         <div class="container">
           <h4 class="font-rubik font-size-20">New Phones</h4>
@@ -22,7 +34,11 @@
                         <div class="price py-2">
                           <span>$<?php echo $item['item_price'] ?? "0" ?></span>
                         </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                        <form method="post">
+                          <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1' ?>">
+                          <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                          <button type="submit" name="new_phones_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                        </form>
                       </div>
                     </div>
                   </div>
