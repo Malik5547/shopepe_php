8b6eb3cb1f9d3907ea8d75850d5f899a0857a7cf
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-29 22:19:43 +0300

    change button

--- template/_products.php
+++ template/_products.php
@@ -17,7 +17,13 @@
                             <button type="submit" class="btn btn-danger form-control">Proceed to Buy</button>
                         </div>
                         <div class="col">
-                            <button type="submit" class="btn btn-warning form-control">Add to Cart</button>
+                            <?php
+                            if(in_array($item['item_id'], $Cart->getCartId($product->getData('cart')) ?? [])){ 
+                              echo '<button type="submit" disabled class="btn btn-success form-control">In the Cart</button>';
+                            } else{
+                              echo '<button type="submit" class="btn btn-warning form-control">Add to Cart</button>';
+                            }
+                            ?>
                         </div>
                     </div>
                 </div>
